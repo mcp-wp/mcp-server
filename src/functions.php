2e4a71a167eb75030440122bb01ab6005989b91f
Pascal Birchler <pascalb@google.com>
2025-06-20 14:14:18 +0200

    PHPStan fix

--- src/functions.php
+++ src/functions.php
@@ -107,6 +107,7 @@ function register_session_post_type(): void {
 		[
 			'label'   => __( 'MCP Sessions', 'mcp' ),
 			'public'  => false,
+			// @phpstan-ignore cast.useless
 			'show_ui' => defined( 'WP_DEBUG' ) && (bool) WP_DEBUG, // For debugging.
 		]
 	);
